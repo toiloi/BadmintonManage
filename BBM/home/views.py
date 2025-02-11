a3f4337aa9bbdc44995d319d36a67d7e48e4f36d
Duong-TieuLy <linh88700@gmail.com>
2025-02-12 03:23:46 +0700

    tui chinh sua role1

--- BBM/home/views.py
+++ BBM/home/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from B_Court.models import Court
+from B_User.models import CourtManager, CourtStaff, Customer
 
 def home(request):
     return render(request, 'home/home.html')
+
+def user_register(request):
+    return render(request, "register.html")
 
 def user_login(request):
     if request.method == "POST":
@@ -14,10 +19,14 @@ def user_login(request):
         
         if user is not None:
             login(request, user)
-            return redirect("role1")
+            if Customer.objects.filter(account=user).exists():
+                return redirect("role1")
+            elif CourtManager.objects.filter(account=user).exists():
+                return redirect("role3")
+            elif CourtStaff.objects.filter(account=user).exists():
+                return redirect("role2")
         else:
             return render(request, "home/login.html", {"error": "Tên đăng nhập hoặc mật khẩu không đúng"})
-    
     return render(request, "home/login.html", {"user":request.user})
 
 def user_logout(request):
@@ -26,11 +35,11 @@ def user_logout(request):
 
 @login_required (login_url="login")
 def role1(request):
-     return render(request, 'home/role1.html')
+    lc = Court.objects.all()
+    return render(request, 'home/role1.html', {"lc":lc})
 
 def role2(request):
-     return render(request, 'home/role2.html')
+    return render(request, 'home/role2.html')
 
 def role3(request):
-     return render(request, 'home/role3.html')
-
+    return render(request, 'home/role3.html')
