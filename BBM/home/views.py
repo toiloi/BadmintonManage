06102ba3fe5f64c497a35f8471d18ce311bd149e
Dieu-Dinh <dieudinh2005@gmail.com>
2025-02-13 08:50:55 +0700

    Them staffManage

--- BBM/home/views.py
+++ BBM/home/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from B_Court.models import Court
 from B_User.models import CourtManager, CourtStaff, Customer
-
+from models import Staff 
 def home(request):
     return render(request, 'home/home.html')
 
@@ -42,5 +42,30 @@ def role2(request):
     return render(request, 'home/role2.html')
 
 def role3(request):
-    return render(request, 'home/role3.html')
+    # Logic cho view role3
+    # return render(request, 'home/role3.html')
 
+    courts = [
+        {'name': 'Court 1', 'price': '100.000/h', 'location': '69, Nguyen Gia Trí, Phuong 25, Quan Binh Thanh', 'size': '5 san', 'capacity': '12 nguoi'},
+        {'name': 'Court 2', 'price': '150.000/h', 'location': '456 Dien Bien Phu, Phuong 25, Quan Binh Thanh', 'size': '7 san', 'capacity': '15 nguoi'},
+        # Thêm các sân khác ở đây
+    ]
+    employees = [
+        {'name': 'A', 'position': 'Manager', 'email': 'A@gmail.com', 'phone': '123-456-7890'},
+        {'name': 'B', 'position': 'Staff', 'email': 'B@gmail.com', 'phone': '098-765-4321'},
+        # Thêm các nhân viên khác ở đây
+    ]
+    context = {
+        'courts': courts,
+        'employees': employees,
+    }
+    return render(request, 'home/role3.html', context)
+
+def staffManage(request):
+    staff =[
+        {'ID': '01','Tên': 'Điểu Đinh', 'Số Điện Thoại':'0358493756', 'Email':'dieu@gmail.com','Trạng Thái':'Active','Chi tiết':'i'}
+    ]
+    context ={
+        'staff':staff,
+    }
+    return render(request,'home/staffManage.html',context)
