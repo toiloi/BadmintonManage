586c29379e3ae85b33648e396a5a0ac61249bb93
toiloi <anhtoai847@gmail.com>
2025-02-16 23:55:12 +0700

    hoàn thiện model

--- BBM/BBooking/models.py
+++ BBM/BBooking/models.py
@@ -4,33 +4,27 @@ from BUser.models import User, CourtStaff
 
 # Create your models here.
 class TimeSlot(models.Model):
-    timeslot = models.DateTimeField()
-    
-class TimeSlotCourt(models.Model):
-    court = models.ForeignKey(Court, on_delete=models.CASCADE)
-    timeslot = models.ForeignKey(TimeSlot, on_delete=models.CASCADE)
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=["court", "timeslot"], name="unique_Timeslot")
-        ]
+    timeslot = models.TimeField(primary_key=True)
     def __str__(self):
         return f"{self.timeslot}"
 
+
 class VeDatSan(models.Model):
-    maVe = models.CharField(default='',max_length=10, primary_key=True)
     thanhToan = models.BooleanField(default=True)
-    san = models.ManyToManyField(San)
-    timeslot = models.ManyToManyField(TimeSlot)
+    san = models.ForeignKey(San, on_delete=models.CASCADE)
+    timeslot = models.ForeignKey(TimeSlot, on_delete=models.CASCADE)
+    date = models.DateField()
     customer = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'role':'customer'})
+    class Meta:
+        unique_together = ('san', 'date', 'timeslot')
     def __str__(self):
-        return f"{self.maVe}"
+        return f"{self.san} {self.date} {self.timeslot}"
 
 class HoaDon(models.Model):
     maHoaDon = models.CharField(default='',max_length=10, primary_key=True)
     tongTien = models.IntegerField(default=0)
     ngayTao = models.DateTimeField()
-    note = models.CharField(default='',max_length=255)
     vedatsan = models.OneToOneField(VeDatSan,on_delete=models.CASCADE)
-    courtStaff = models.ForeignKey(CourtStaff, on_delete=models.CASCADE)
+    courtStaff = models.ForeignKey(CourtStaff, on_delete=models.CASCADE, blank=True, null=True)
     def __str__(self):
         return f"{self.maHoaDon} {self.ngayTao}"
