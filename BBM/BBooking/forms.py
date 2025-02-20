2c226bfe8748c10f1a965e1dd6054eea2deae3f0
hhoang205 <nguyenhuyhoang2052005@gmail.com>
2025-02-20 14:44:19 +0700

    cap nhat them selenium

--- BBM/BBooking/forms.py
+++ BBM/BBooking/forms.py
@@ -10,3 +10,13 @@ class FlagForm(forms.ModelForm):
     class Meta:
         model = Flag
         fields = ['san', 'timeslot', 'date']
+
+class Gangzbit(forms.ModelForm):
+    class Meta:
+        model = Voucher
+        fields = ['voucher','court','percent']
+        widgets = {
+            'voucher': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nhập mã khuyến mãi'}),
+            'court': forms.Select(attrs={'class': 'form-control'}),
+            'percent': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Nhập phần trăm giảm giá', 'min': 0, 'max': 40}),
+        }
