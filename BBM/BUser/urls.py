f807e4359fe3ba7a2079ec6ab863b10f8efe99c6
toiloi <anhtoai847@gmail.com>
2025-02-20 12:43:15 +0700

    xong role 1 role 2

--- BBM/BUser/urls.py
+++ BBM/BUser/urls.py
@@ -4,7 +4,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('changeinf/', views.ChangeInf, name='changeinf'),
+    path('changeinf2/', views.ChangeInf2, name='changeinf2'),
     path('userdetail/', views.userDetail, name='userdetail'),
+    path('userdetail2/', views.userDetail2, name='userdetail2'),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='home/changepas.html'), name='password_change'),
+    path('password_change2/', auth_views.PasswordChangeView.as_view(template_name='home/changepas2.html'), name='password_change2'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='home/changepasdone.html'), name='password_change_done'),
 ]
--- BBM/BUser/views.py
+++ BBM/BUser/views.py
@@ -6,6 +6,10 @@ def userDetail(request):
     user=request.user
     return render(request, "home/userdetail.html", {'user':user})
 
+def userDetail2(request):
+    user=request.user
+    return render(request, "home/r2userdetail.html", {'user':user})
+
 def ChangeInf(request):
     user = request.user
     form=UserForm(request.POST, instance=user)
@@ -14,3 +18,12 @@ def ChangeInf(request):
             form.save()
             return redirect('userdetail')    
     return render(request, "home/useredit.html", {'form':form})
+
+def ChangeInf2(request):
+    user = request.user
+    form=UserForm(request.POST, instance=user)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('userdetail')    
+    return render(request, "home/r2useredit.html", {'form':form})
